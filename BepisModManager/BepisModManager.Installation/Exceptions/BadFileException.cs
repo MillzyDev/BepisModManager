41238cb638f911cf051ddf029f2e87a0d55bfd85
Millzy <millzyyt08@gmail.com>
2022-02-19 23:38:42 +0000

    exceptions

---
+++ BepisModManager/BepisModManager.Installation/Exceptions/BadFileException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BepisModManager.Installation.Exceptions
+{
+    class BadFileException : Exception
+    {
+        public BadFileException()
+        { 
+        }
+
+        public BadFileException(string message) : base(message)
+        {
+        }
+
+        public BadFileException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
---
+++ BepisModManager/BepisModManager.Installation/Exceptions/NonUnityGameException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BepisModManager.Installation.Exceptions
+{
+    class NonUnityGameException : Exception
+    {
+        public NonUnityGameException()
+        {
+        }
+
+        public NonUnityGameException(string message) : base(message)
+        {
+        }
+
+        public NonUnityGameException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
