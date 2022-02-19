312bdcf572c596120b391d3c5e1d0d929af477c8
Millzy <millzyyt08@gmail.com>
2022-02-19 23:39:17 +0000

    game registry
    handles all the modded games

---
+++ BepisModManager/BepisModManager.Installation/GameRegistry.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace BepisModManager.Installation
+{
+    class GameRegistry
+    {
+        private List<UnityGame> games;
+
+        private GameRegistry instance;
+
+        public GameRegistry Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new GameRegistry();
+                return instance;
+            }
+        }
+
+        public void AddGameLibrary(string path)
+        {
+            string[] gameDirs = Directory.GetDirectories(path);
+
+            try
+            {
+                foreach (string dir in gameDirs)
+                {
+                    AddGame(dir);
+                }
+            }
+            catch(Exception) 
+            {
+                // game couldnt be added for whatever reason, most likely cuz its not a unity game
+            }
+        }
+
+        public void AddGame(string path)
+            => games.Add(new UnityGame(path));
+        
+    }
+}
