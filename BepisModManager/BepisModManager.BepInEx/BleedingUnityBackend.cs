ae9fd97db13171bc83e55e313928012b990f03ce
Millzy <millzyyt08@gmail.com>
2022-02-19 11:01:01 +0000

    backend enums

---
+++ BepisModManager/BepisModManager.BepInEx/BleedingUnityBackend.cs
@@ -0,0 +1,11 @@
+﻿namespace BepisModManager.BepInEx
+{
+    public enum BleedingUnityBackend
+    {
+        BepInEx_UnityMono_x64,
+        BepInEx_UnityMono_x86,
+        BepInEx_UnityMono_unix,
+        BepInEx_UnityIL2CPP_x64,
+        BepInEx_UnityIL2CPP_x86,
+    }
+}
---
+++ BepisModManager/BepisModManager.BepInEx/ReleaseUnityBackend.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BepisModManager.BepInEx
+{
+    public enum ReleaseUnityBackend
+    {
+        BepInEx_x64,
+        BepInEx_x86,
+        BepInEx_unix,
+    }
+}
