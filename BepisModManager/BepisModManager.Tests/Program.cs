341d8e885a78b2559795c374802f14bb780816d7
Millzy <millzyyt08@gmail.com>
2022-02-19 11:01:32 +0000

    ::BepInEx test

---
+++ BepisModManager/BepisModManager.Tests/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepisModManager.BepInEx;
+
+namespace BepisModManager.Tests
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            BleedingArtifacts bleeding = BleedingEdge.GetLatestArtifacts();
+            BleedingArtifacts oldBleeding = BleedingEdge.GetArtifactsWithId("520");
+            ReleaseArtifacts release = Release.GetLatestArtifacts();
+            ReleaseArtifacts oldRelease = Release.GetArtifactsWithVersion("5.4.12");
+
+
+            Console.WriteLine(
+                $"Latest Bleeding Version: b{bleeding.Id}\nb520 Download URL: {BleedingEdge.GetDownloadURL(oldBleeding.Id, oldBleeding.Artifacts[0].File)}\nLatest Release Version: {release.Version}\nv5.4.12 Download URL: {oldRelease.artifacts[0].DownloadURL}"
+                ); ;
+        }
+    }
+}
