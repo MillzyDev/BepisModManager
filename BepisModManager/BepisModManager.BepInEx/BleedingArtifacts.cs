54d32a5060bcff65c75e15fd1039123848c3c31a
Millzy <millzyyt08@gmail.com>
2022-02-19 11:00:08 +0000

    create serializable json object for artifacts

---
+++ BepisModManager/BepisModManager.BepInEx/BleedingArtifacts.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json;
+using System.Linq;
+
+namespace BepisModManager.BepInEx
+{
+    public class BleedingArtifacts
+    {
+        public class BleedingArtifact
+        {
+            [JsonProperty("description")]
+            public string Description
+            {
+                get;
+                set;
+            }
+
+            [JsonProperty("file")]
+            public string File
+            {
+                get;
+                set;
+            }
+        }
+
+        public BleedingArtifact GetArtifact(BleedingUnityBackend backend)
+        {
+            string stringValue = backend.ToString();
+            return Artifacts.First(x => x.File.StartsWith(stringValue));
+        }
+               
+        [JsonProperty("artifacts")]
+        public BleedingArtifact[] Artifacts
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("changelog")]
+        public string Changelog
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("date")]
+        public string Date
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("hash")]
+        public string Hash
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("id")]
+        public string Id
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("short_hash")]
+        public string ShortHash
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ BepisModManager/BepisModManager.BepInEx/ReleaseArtifacts.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace BepisModManager.BepInEx
+{
+    public class ReleaseArtifacts
+    {
+        public class ReleaseArtifact
+        {
+            [JsonProperty("browser_download_url")]
+            public string DownloadURL
+            {
+                get;
+                set;
+            }
+
+            [JsonIgnore]
+            ReleaseUnityBackend backend;
+
+            [JsonIgnore]
+            public ReleaseUnityBackend Backend { 
+                get
+                {
+                    if (backend == null)
+                    {
+                        string[] url = DownloadURL.Split('/');
+                        string file = url[url.Length - 1];
+                        string[] components = file.Split('_');
+                        string backendStr = String.Join("_", components[0], components[1]);
+                        if (!Enum.TryParse(backendStr, out backend))
+                        {
+                            backend = ReleaseUnityBackend.BepInEx_x64;
+                        }
+                    }
+                    return backend;
+                }
+            }
+        }
+
+        [JsonIgnore]
+        string version;
+
+        [JsonProperty("tag_name")]
+        public string Version
+        {
+            get => version;
+            set => version = value.Replace("v", "");
+        }
+
+        [JsonProperty("assets")]
+        public ReleaseArtifact[] artifacts;
+    }
+}
