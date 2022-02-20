dcb73e756e0560231e70b06341663dc8efc5e689
Millzy <millzyyt08@gmail.com>
2022-02-20 22:29:50 +0000

    temp dir stuff

---
+++ BepisModManager/BepisModManager.Installation/TempStorage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BepisModManager.Installation
+{
+    static class TempStorage
+    {
+        public static string TempPath
+        {
+            get => Path.Combine(Path.GetTempPath(), "BepisModManager");
+        }
+    }
+}
